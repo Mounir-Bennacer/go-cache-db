b156b9268288789df89c3b3cf7a84bf1ae8d2eb5
Mounir Bennacer <benphaoris@gmail.com>
2024-05-26 19:55:00 +0100

    feat: configuring the server to receive commands from the client

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Starting server on port 6379")
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close() // close connection when done
+
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading from client: ", err.Error())
+			os.Exit(1)
+		}
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
